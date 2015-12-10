bb60f5067079dd00bf2f32d869819d029e4fe073
Alice Pote <alice.writes.wrongs@gmail.com>
2015-12-09 19:51:41 -0500

    requirements to run the script

--- eviction.py
+++ eviction.py
@@ -1,6 +1,21 @@
 import csv
+import random
+import sqlite3
+import os
 
 class RandomEviction(object):
     def __init__(self, params):
         self.params = params
+        self.write_csv_to_sqlite()
+
+    def write_csv_to_sqlite(self):
+        if os.path.isfile("./addresses.db")
+            return None
+        conn = sqlite3.connect("./addresses.db")
+        conn.text_factory = str
+        cur = conn.cur()
+
+
+
+    def generate(self):
 
