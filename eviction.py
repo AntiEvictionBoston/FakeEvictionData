0b36cd0ce65e324fe3532e1223d2b8d4278c4717
Alice Pote <alice.writes.wrongs@gmail.com>
2015-12-09 20:27:41 -0500

    close to what we want

--- eviction.py
+++ eviction.py
@@ -9,6 +9,12 @@ class RandomEviction(object):
         self.params = params
 
     def generate(self):
-        print("this")
-            
+        if not os.path.isfile("addresses.db")
+            raise FileNotFoundError("Did you generate the sqlite database yet?")
+        connection = sqlite3.connect("addresses.db")
+        c = connection.cursor()
 
+        row = c.execute('SELECT * FROM address ORDER BY RANDOM() LIMIT 1').fetchone()
+
+
+        connection.close()
