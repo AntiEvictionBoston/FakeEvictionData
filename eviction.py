c91f62a85650c588eb378e3fc036cd65daa47333
Alice Pote <alice.writes.wrongs@gmail.com>
2015-12-10 07:28:12 -0500

    this is pretty much what we want

--- eviction.py
+++ eviction.py
@@ -2,19 +2,26 @@ import csv
 import random
 import sqlite3
 import os
-import subprocess
+import radar
 
 class RandomEviction(object):
     def __init__(self, params):
         self.params = params
+        with open(params["csv_file"]) as f:
+            self.headers = csv.DictReader(f).fieldnames
 
     def generate(self):
-        if not os.path.isfile("addresses.db")
+        eviction = {}
+        if not os.path.isfile("addresses.db"):
             raise FileNotFoundError("Did you generate the sqlite database yet?")
         connection = sqlite3.connect("addresses.db")
         c = connection.cursor()
-
         row = c.execute('SELECT * FROM address ORDER BY RANDOM() LIMIT 1').fetchone()
-
-
+        for key, value in zip(headers, row) :
+            eviction[key] = value
         connection.close()
+        eviction["date"] = radar.random_date(
+                start = params["start_date"],
+                stop = params["end_date"]
+                )
+        return eviction
