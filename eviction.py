4533457ed928b3258e02b591c230be16f642fdb7
Alice Pote <alice.writes.wrongs@gmail.com>
2015-12-09 20:13:37 -0500

    we don't need to do that in Python

--- eviction.py
+++ eviction.py
@@ -2,20 +2,13 @@ import csv
 import random
 import sqlite3
 import os
+import subprocess
 
 class RandomEviction(object):
     def __init__(self, params):
         self.params = params
-        self.write_csv_to_sqlite()
-
-    def write_csv_to_sqlite(self):
-        if os.path.isfile("./addresses.db")
-            return None
-        conn = sqlite3.connect("./addresses.db")
-        conn.text_factory = str
-        cur = conn.cur()
-
-
 
     def generate(self):
+        print("this")
+            
 
